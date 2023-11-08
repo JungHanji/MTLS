5730f7c6b0647982524bf840ebe09e27062e13a7
JungHanji <88892247+JungHanji@users.noreply.github.com>
2023-11-08 19:18:18 +0300

    returned a ptrs

--- src/include/AdtClasses/Async.hpp
+++ src/include/AdtClasses/Async.hpp
@@ -34,13 +34,12 @@ function<void(vector<T>&, vector<T>&)> makeLocking(function<void(vector<T>&, vec
 template<class RT, class PT, class... Args>
 class future{
     private:
-    std::unique_ptr<std::thread> this_thread;
+    std::thread* this_thread;
     function<RT(PT, Args...)> func;
     function<void(RT*, PT, Args...)> wrapped;
     function<void(RT*, PT, Args...)> wrapped_locking;
     mutex this_mutex;
     RT output;
-
 
     bool is_locking = false,
          is_deffered = false;
@@ -55,31 +54,32 @@ class future{
         if(!is_deffered){
         if(!is_locking){
             wrapped = getWrapped<RT, PT, Args...>(func);
-            this_thread = std::make_unique<std::thread>(wrapped, &output, arg, args...);
+            this_thread = new std::thread(wrapped, &output, arg, args...);
         } else {
             wrapped_locking = getLockingWrapped<RT, PT, Args...>(func, &this_mutex);
-            this_thread = std::make_unique<std::thread>(wrapped_locking, &output, arg, args...);
+            this_thread = new std::thread(wrapped_locking, &output, arg, args...);
         }
         }
     }
     future(){;}
 
     RT get(){
-        if(!is_deffered)
+        if(!is_deffered){
             this_thread->join();
+            return output;}
         else
             throw custom_exception("Cannot use method get(void) with deffered call");
-        return output;
+        
     }
 
     RT get(PT arg, Args... args){
         if(is_deffered){
             if(!is_locking){
                 wrapped = getWrapped<RT, PT, Args...>(func);
-                this_thread = std::make_unique<std::thread>(wrapped, &output, arg, args...);
+                this_thread = new std::thread(wrapped, &output, arg, args...);
             } else {
                 wrapped_locking = getLockingWrapped<RT, PT, Args...>(func, &this_mutex);
-                this_thread = std::make_unique<std::thread>(wrapped_locking, &output, arg, args...);
+                this_thread = new std::thread(wrapped_locking, &output, arg, args...);
             }
 
             this_thread->join();
@@ -91,13 +91,14 @@ class future{
 
     ~future() {
         this_thread->detach();
+        this_thread.reset();
     }
 };
 
 template<class T>
 class _queue{
     private:
-    std::unique_ptr<std::thread> this_thread;
+    std::thread* this_thread;
     std::mutex this_mutex;
     vector<T> &input;
     vector<T> &output;
@@ -108,7 +109,7 @@ class _queue{
     _queue(vector<T> &input, vector<T> &output, function<void(vector<T>&, vector<T>&)> async_handler):
            input(input), output(output), async_handler(async_handler){
         async_handler = makeLocking(async_handler, &this_mutex);
-        this_thread = std::make_unique<std::thread>(async_handler, std::ref(input), std::ref(output));
+        this_thread = new std::thread(async_handler, std::ref(input), std::ref(output));
     }
 
     void run_detached(){ this_thread->detach(); }
@@ -120,6 +121,7 @@ class _queue{
         if (this_thread->joinable()) {
             this_thread->detach();
         }
+        this_thread.reset();
     }
 };
 
